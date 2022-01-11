4825245cb59543669cd2efdbb1b2ffce4252092f
HackrJi <60073425+iHackerJi@users.noreply.github.com>
2022-01-11 19:06:15 +0800

    1

--- HyperFrog/vt/hook/hook_table.h
+++ HyperFrog/vt/hook/hook_table.h
@@ -9,7 +9,7 @@ typedef struct _MsrHookTable
 
 PFN_NtOpenProcess orgNtOpenProcess;
 static bool g_MsrHookEnableTable[MAX_SYSCALL_INDEX] = { 0 };
-DECLSPEC_CACHEALIGN KSERVICE_TABLE_DESCRIPTOR g_KeServiceDescriptorTable[NUMBER_SERVICE_TABLES];
+static DECLSPEC_CACHEALIGN KSERVICE_TABLE_DESCRIPTOR g_KeServiceDescriptorTable[NUMBER_SERVICE_TABLES] = {0};
 static MsrHookTable g_MsrHookTable[] =
 {
     {
